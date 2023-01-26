10703c996e493c56bb1d6de9ddc600b540cf42eb
KravchukOleh <olehkr93@gmail.com>
2023-01-26 23:14:34 +0200

    "lessons 7"

--- public_html/core/base/controller/RouteController.php
+++ public_html/core/base/controller/RouteController.php
@@ -25,56 +25,138 @@ class RouteController
 
   private function _clone()
   {
+
   }
   static public function getInstance(){
+
       if(self::$_instance instanceof self){
+
           return self::$_instance;
       }
+
       return self::$_instance = new self;
   }
     private function  _construct()
     {
         $adress_str =$_SERVER['REQUEST_URI'];
+
         if(strrpos($adress_str, '/') === strlen($adress_str)-1 && strrpos($adress_str, '/') !== 0){
-        $this->redirect(rtrim($adress_str,'/'), 301);
+
+            $this->redirect(rtrim($adress_str,'/'), 301);
     }
     $path = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'],'index.php'));
-        if ($path === PATH){
-            $this->routes = Settings::get('routers');
-            if(!$this->routes) throw new RouteException('Сайт знаходиться на технічному обслуговуванні');
-            if(strrpos($adress_str, $this->routes['admin']['alias']) === strlen("PATH")){
 
+        if ($path === PATH){
+
+            $this->routes = Settings::get('routers');
+
+            if(!$this->routes) throw new RouteException('Сайт знаходиться на технічному обслуговуванні');
+
+            if(strpos($adress_str, $this->routes['admin']['alias']) === strlen("PATH")){
+
+                $url = explode('/', substr($adress_str, strlen(PATH . $this->routes['admin']['alias'])+1));
+
+                if($url[0] && is_dir($_SERVER['DOCUMENT_ROOT'] . PATH . $this->routes['plugins']['path'] . $url[0])){
+
+                    $plugin = array_shift($url);
+
+                    $pluginSettings = $this->routes['settings']['path'] . ucfirst($plugin . 'Settings');
+
+                if(file_exists($_SERVER['DOCUMENT_ROOT'] . PATH . $pluginSettings . '.php')){
+
+                    $pluginSettings = str_replace('/','\\',$pluginSettings);
+
+                    $this->routes = $pluginSettings::get('routes');
+                }
+                $dir = $this->routes['plugins']['dir'] ? '/' . $this->routes['$plugin']['dir'] . '/' : '/';
+
+                $dir = str_replace('//','/', $dir);
+
+               $this->controller = $this->routes['admin']['path'] . $plugin . $dir;
+
+               $hrUrl = $this->routes['plugins']['hrUrl'];
+
+                    $route = 'plugins';
+                }else{
+                    $this->controller = $this->routes['admin']['path'];
+
+                    $hrUrl = $this->routes['admin']['hrUrl'];
+
+                    $route = 'admin';
+                }
             }else{
+
                 $url = explode('/', substr($adress_str, strlen(PATH)));
+
             $hrUrl = $this->routes['user']['hrUrl'];
+
             $this->controller = $this->routes['user']['path'];
+
             $route = 'user';
+
             }
 
             $this->createRoute($route, $url);
-            exit();
+            if($url[1]){
+                $count = count($url);
+                $key = '';
+                if(!$hrUrl) {
+                    $i = 1;
+                }else {
+                    $this->parameters['alias'] = $url[1];
+                    $i = 2;
+                }
+            for ( ; $i<$count; $i++){
+                if(!$key) {
+                    $key = $url[$i];
+                    $this->parameters[$key] = '';
+                }else{
+                    $this->parameters[$key] = $url[$i];
+                    $key = '';
+                }
+            }
+            }
+                exit();
 
         }else{
+
             try {
+
                 throw new \Exception('Некоректна директорія сайту');
+
             }catch (\Exception $e){
+
                 exit($e->getMessage());
             }
         }
     }
+
             private function createRoute($var, $arr){
+
       $route =[];
+
       if(!empty($arr[0])){
+
           if($this->routes[$var]['routes'][$arr[0]]){
+
               $route = explode('/',$this->routes[$var]['routes'][$arr[0]]);
+
               $this->controller .= ucfirst($route[0].'Controller');
+
           }else{
+
               $this->controller .= ucfirst($arr[0].'Controller');
+
           }
+
       }else{
+
           $this->controller .= $this->routes['default']['controller'];
+
       }
+
       $this->inputMethod = $route[1] ? $route [1] : $this->routes['default']['inputMethod'];
+
       $this->outputMethod = $route[1] ? $route [1] : $this->routes['default']['outputMethod'];
 
       return;
